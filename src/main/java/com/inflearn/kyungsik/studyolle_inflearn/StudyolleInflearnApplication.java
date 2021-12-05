8ed24133a40e91f3c6bc129b6eb46eb25d423b0d
KyungSik9870 <rudtlr9870@gmail.com>
2021-12-05 23:59:25 +0900

    Initial commit

---
+++ src/main/java/com/inflearn/kyungsik/studyolle_inflearn/StudyolleInflearnApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn.kyungsik.studyolle_inflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyolleInflearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyolleInflearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inflearn/kyungsik/studyolle_inflearn/StudyolleInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn.kyungsik.studyolle_inflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyolleInflearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
