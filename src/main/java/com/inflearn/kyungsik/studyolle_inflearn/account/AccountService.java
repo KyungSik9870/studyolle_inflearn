a8adf0ca3f2210b858065b6e72eb42f54e098a50
KyungSik9870 <rudtlr9870@gmail.com>
2022-01-07 19:52:43 +0900

    feat : 로그인 로그아웃

--- src/main/java/com/inflearn/kyungsik/studyolle_inflearn/account/AccountService.java
+++ src/main/java/com/inflearn/kyungsik/studyolle_inflearn/account/AccountService.java
@@ -7,6 +7,9 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,8 +20,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class AccountService {
-
+public class AccountService implements UserDetailsService {
 	private final JavaMailSender javaMailSender;
 	private final AccountRepository accountRepository;
 	private final PasswordEncoder passwordEncoder;
@@ -49,6 +51,18 @@ public class AccountService {
 			List.of(new SimpleGrantedAuthority("ROLE_USER")));
 
 		SecurityContextHolder.getContext().setAuthentication(token);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String emailOrNickname) throws UsernameNotFoundException {
+		Account account = accountRepository.findByEmail(emailOrNickname);
+		if (account == null) {
+			account = accountRepository.findByNickname(emailOrNickname);
+		}
+		if(account == null) {
+			throw new UsernameNotFoundException(emailOrNickname);
+		}
+		return new UserAccount(account);
 	}
 
 	private Account saveNewAccount(SignUpForm signUpForm) {
